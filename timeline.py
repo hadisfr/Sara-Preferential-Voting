7c86611fc20aa0f767edf6135d4c0ea7fe05b8f2
H@di <hadi@hadisafari.ir>
2021-06-11 19:49:15 +0430

    add timeline

---
+++ timeline.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import json
+from datetime import datetime
+from collections import Counter
+
+from matplotlib import pyplot as plt
+
+
+def get_list(addr):
+    with open(addr) as f:
+        return set(map(str.strip, f))
+
+
+def filter_votes(votes, func):
+    return map(lambda vote: list(filter(func, vote)), votes)
+
+
+def get_votes(addr, valid_candidates):
+    with open(addr) as f:
+        votes = json.load(f)
+    print("read %d votes from source" % len(votes))
+    votes = filter(lambda itm: "votes" in itm["body"], votes)
+    votes = map(lambda votes: datetime.fromtimestamp(int(votes["datetime"])/1000), votes)
+    votes = map(lambda dt: datetime.strftime(dt, "%d %H"), votes)
+    return list(votes)
+
+
+def main():
+    valid_candidates = get_list("list.txt")
+    print("%d valid candidates" % len(valid_candidates))
+
+    votes = get_votes("output.json", valid_candidates)
+    total_voters = len(votes)
+    print("%d valid votes" % total_voters)
+
+    c = Counter(votes)
+    xs = ["10 %2d" % h for h in range(12, 24)] + ["11 %2d" % h for h in range(0, 21)]
+
+    fig = plt.figure(figsize=(15, 5))
+    plt.plot(xs, [c.get(x, 0) for x in xs])
+    plt.xlabel("زمان")
+    plt.ylabel("رأی در ساعت")
+    fig.autofmt_xdate()
+    plt.grid()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
